bad5d0b1dcee64588188f5a4a3f01874d9d17f4d
CDWimmer <31413434+CDWimmer@users.noreply.github.com>
2026-09-16 03:04:17 +0100

    Wallet Slot
    Requires sacrificing the disused leg slot (unless we want to make the slot flag more than just a byte?). Extend inventory templates for regular character species, plus monkeys and scurrets. Not massively happy about the stripping slot placement but whaddyagonna do?

--- Content.Shared/Inventory/SlotFlags.cs
+++ Content.Shared/Inventory/SlotFlags.cs
@@ -23,7 +23,9 @@ public enum SlotFlags
     GLOVES = 1 << 10,
     IDCARD = 1 << 11,
     POCKET = 1 << 12,
-    LEGS = 1 << 13,
+    //  Carpmosia-start - legs aren't used, reduce reuse recycle for wallet slot
+    // LEGS = 1 << 13,
+    WALLET = 1 << 13,
     FEET = 1 << 14,
     SUITSTORAGE = 1 << 15,
     All = ~NONE,
